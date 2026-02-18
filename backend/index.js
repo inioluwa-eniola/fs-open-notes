0fe236c700aa588fd36c2d279302ed481af135d6
Inioluwa Eniola <inioluwae04@gmail.com>
2026-02-18 00:31:39 -0500

    main

---
+++ backend/index.js
@@ -0,0 +1,71 @@
+require('dotenv').config()
+console.log('MONGODB_URI:', process.env.MONGODB_URI)
+const express = require('express')
+const Note = require('./models/note')
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/api/notes', (request, response) => {
+    Note.find({}).then(notes => {
+        response.json(notes)
+    })
+})
+
+app.get('/api/notes/:id', (request, response) => {
+    Note.findById(request.params.id)
+        .then(note => {
+            if (note) {
+                response.json(note)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => {
+            console.log(error)
+            response.status(400).send({ error: 'malformatted id' })
+        })
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+    Note.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => {
+            console.log(error)
+            response.status(400).send({ error: 'malformatted id' })
+        })
+
+})
+
+app.post ('/api/notes', (request, response) => {
+    const body = request.body
+
+    if (!body.content) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    const note = new Note({
+        content: body.content,
+        important: body.important || false,
+    })
+
+    note.save().then(savedNote => {
+        response.json(savedNote)
+    })
+
+})
+
+app.use(express.static('dist'))
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
+
+
+
